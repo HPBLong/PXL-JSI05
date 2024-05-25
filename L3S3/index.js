aaf8c917096db9b04487b9577582a58c25e36a82
Long <phanbaolong0306@gmail.com>
2024-05-25 19:09:27 +0700

    new lesson

---
+++ L3S3/index.js
@@ -0,0 +1,11 @@
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.1/firebase-app.js";
+const firebaseConfig = {
+  apiKey: "AIzaSyCHggMOZKTspn5FKC4Fx5SZhPXwmUWdA6M",
+  authDomain: "l2s3-a38c0.firebaseapp.com",
+  projectId: "l2s3-a38c0",
+  storageBucket: "l2s3-a38c0.appspot.com",
+  messagingSenderId: "970565908689",
+  appId: "1:970565908689:web:14ddce625e08a578f06df0",
+};
+
+const app = initializeApp(firebaseConfig);
